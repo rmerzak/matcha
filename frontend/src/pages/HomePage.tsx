a155d3ea017d80057a5aa21500248ab65746b0db
Mehdi Akacem <makacem@e1r2p1.1337.ma>
2025-02-23 10:28:17 +0000

    added Sidebar

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,14 @@
-import useAuthStore from "../store/useAuthStore";
+import { Sidebar } from "../components/Sidebar";
 
 type Props = {};
 
 function HomePage({}: Props) {
-  const { signOut } = useAuthStore();
   return (
-    <div className="flex justify-between">
-      HomePage
-      <button className="border p-2 border-black" onClick={signOut}>sign Out</button>
+    <div
+      className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-pink-100 to bg-purple-100
+    overflow-hidden"
+    >
+      <Sidebar />
     </div>
   );
 }
