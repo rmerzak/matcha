cd06b78afcedbb1f5d824f74fdd8041f5c360b5e
Mehdi Akacem <makacem@e1r2p1.1337.ma>
2025-02-24 10:51:16 +0000

    swapped last name and first name

--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -67,6 +67,30 @@ export default function SignUpForm() {
         </div>
       </div>
 
+      {/* FIRSTNAME */}
+      <div>
+        <label
+          htmlFor="firstname"
+          className="block text-sm font-medium text-gray-700"
+        >
+          First name
+        </label>
+        <div className="mt-1">
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            autoComplete="firstname"
+            required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md
+			shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 
+			focus:border-blue-500 sm:text-sm"
+          />
+        </div>
+      </div>
+
       {/* LASTNAME */}
       <div>
         <label
@@ -91,29 +115,6 @@ export default function SignUpForm() {
         </div>
       </div>
 
-      {/* FIRSTNAME */}
-      <div>
-        <label
-          htmlFor="firstname"
-          className="block text-sm font-medium text-gray-700"
-        >
-          First name
-        </label>
-        <div className="mt-1">
-          <input
-            type="text"
-            id="firstname"
-            name="firstname"
-            autoComplete="firstname"
-            required
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md
-			shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 
-			focus:border-blue-500 sm:text-sm"
-          />
-        </div>
-      </div>
 
       {/* PASSWORD  */}
       <div>
