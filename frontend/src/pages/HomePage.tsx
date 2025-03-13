f12be3b9527e011ce6000f2e368ad89bae67a596
mehdiakacem <mehdiakacem20@gmail.com>
2025-03-13 20:50:39 +0000

    added filters button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Sidebar } from "../components/Sidebar";
 import { useMatchStore } from "../store/useMatchStore";
 import { Header } from "../components/Header";
-import { Frown, MapPin, Star } from "lucide-react";
+import { ChevronDown, Frown, MapPin, Star } from "lucide-react";
 import { users } from "../users";
 import { Link } from "react-router-dom";
 
@@ -22,67 +22,71 @@ function HomePage() {
       <Sidebar />
       <div className="flex flex-grow flex-col overflow-hidden">
         <Header />
-        <main className="flex-grow flex flex-col gap-10 p-4 relative overflow-hidden">
-          {users.length > 0 && !isLoadingUserProfiles && (
-            <div>
-              <div className="border border-black flex justify-end">
-                <div className="">filter</div>
-              </div>
-            <div className="flex lg:flex-col gap-2 flex-wrap">
-              {/* <>Users found</> */}
-              {users.slice(0, 19).map((user, index) => (
-                <div
-                  key={index}
-                  className="w-40 border bg-white p-2 rounded-lg border-gray-200 shadow-sm
+        <div className="flex flex-col">
+          <button className="flex divide-x  divide-gray-400 justify-end border border-gray-400">
+            <div></div>
+            <div className="text-sm px-6 py-2 text-purple-700 flex items-center gap-1 font-semibold">
+              <span className="">Filters</span>
+              <ChevronDown className="size-3" />
+            </div>
+          </button>
+          <main className="flex-grow flex flex-col gap-10 p-4 relative overflow-hidden">
+            {users.length > 0 && !isLoadingUserProfiles && (
+              <div className="flex lg:flex-col gap-2 flex-wrap">
+                {/* <>Users found</> */}
+                {users.slice(0, 19).map((user, index) => (
+                  <div
+                    key={index}
+                    className="w-40 border bg-white p-2 rounded-lg border-gray-200 shadow-sm
                   lg:w-full lg:flex lg:space-x-4"
                   >
-                  <img
-                    src={user.profilePicture}
-                    className="h-44 w-44 object-cover rounded-lg "
-                    alt=""
+                    <img
+                      src={user.profilePicture}
+                      className="h-44 w-44 object-cover rounded-lg "
+                      alt=""
                     />
-                  <div className="flex flex-col">
-                    <span className="text-xl font-bold mb-1">
-                      {user.firstName} {user.lastName}
-                    </span>
-                    <div className="flex flex-col text-gray-700 text-sm">
-                      <div className="flex space-x-1 items-center">
-                        <MapPin className="size-4" />{" "}
-                        <span>{user.location}</span>
+                    <div className="flex flex-col">
+                      <span className="text-xl font-bold mb-1">
+                        {user.firstName} {user.lastName}
+                      </span>
+                      <div className="flex flex-col text-gray-700 text-sm">
+                        <div className="flex space-x-1 items-center">
+                          <MapPin className="size-4" />{" "}
+                          <span>{user.location}</span>
+                        </div>
+                        <div className="flex space-x-1 ">
+                          <Star className="size-4" />{" "}
+                          <span>Fame rating: {6.6}</span>
+                        </div>
                       </div>
-                      <div className="flex space-x-1 ">
-                        <Star className="size-4" />{" "}
-                        <span>Fame rating: {6.6}</span>
-                      </div>
-                    </div>
-                    <div className="mt-2 flex flex-wrap gap-1">
-                      {user.interests.map((interest: string, index) => (
-                        <span
-                          key={index}
-                          className="border rounded-2xl text-xs px-2 inline-block"
+                      <div className="mt-2 flex flex-wrap gap-1">
+                        {user.interests.map((interest: string, index) => (
+                          <span
+                            key={index}
+                            className="border rounded-2xl text-xs px-2 inline-block"
                           >
-                          {interest}
-                        </span>
-                      ))}
-                    </div>
-                    <Link
-                      to={`/users/${user.username}`}
-                      className=" px-4 py-2 rounded-full font-semibold
+                            {interest}
+                          </span>
+                        ))}
+                      </div>
+                      <Link
+                        to={`/users/${user.username}`}
+                        className=" px-4 py-2 rounded-full font-semibold
                       text-sm bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2
                       focus:ring-offset-2 focus:ring-purple-500 text-center mt-3 text-black
                       "
                       >
-                      View profile
-                    </Link>
+                        View profile
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              ))}
-            </div>
+                ))}
               </div>
-          )}
-          {users.length === 0 && !isLoadingUserProfiles && <NoMoreProfiles />}
-          {isLoadingUserProfiles && <LoadingUI />}
-        </main>
+            )}
+            {users.length === 0 && !isLoadingUserProfiles && <NoMoreProfiles />}
+            {isLoadingUserProfiles && <LoadingUI />}
+          </main>
+        </div>
       </div>
     </div>
   );
